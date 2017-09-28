e392c21f487787f8bb241b7a2dabc1bd38745e2e
Nafaa Friaa <nafaa.friaa@isetjb.rnu.tn>
2017-09-28 16:41:40 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # hibernate-tutorial1
-Hibernate tutorial 1 : Configuration
+Hibernate tutorial 1 : JavaSE + Hibernate + Maven + MySQL Basic configuration
+
+
---
+++ src/main/java/net/isetjb/hibernatetutorial1/Application.java
@@ -0,0 +1,56 @@
+package net.isetjb.hibernatetutorial1;
+
+import java.util.List;
+import java.util.Random;
+import org.hibernate.Session;
+
+/**
+ * Application class.
+ *
+ * @author Nafaa Friaa (nafaa.friaa@isetjb.rnu.tn)
+ */
+public class Application
+{
+
+    public static void main(String[] args)
+    {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+
+        ///// SQL queries
+        // Single result :
+        String sql1 = "SELECT VERSION()";
+        String result1 = (String) session.createNativeQuery(sql1).getSingleResult();
+        System.out.println("Result1 ==> " + result1);
+
+        // List result :
+        String sql2 = "SHOW TABLES";
+        List result2 = session.createNativeQuery(sql2).getResultList();
+        System.out.println("Result2 ==> ");
+        for (Object temp : result2)
+        {
+            System.out.println(temp);
+        }
+
+        // Insert record :
+        int randomNumber = new Random().nextInt(100);
+        String sql3 = "INSERT INTO product VALUES(null, 'Qwerty_" + randomNumber + "' , " + randomNumber + ")";
+        int result3 = (int) session.createNativeQuery(sql3).executeUpdate();
+        System.out.println("Result3 ==> " + result3);
+
+        // List array result :
+        String sql4 = "SELECT * FROM product";
+        List<Object[]> result4 = session.createNativeQuery(sql4).getResultList();
+        System.out.println("Result4 ==> ");
+        for (Object[] temp : result4)
+        {
+            System.out.println("id:" + temp[0] + " ===> name:" + temp[1] + " ===> price:" + temp[2]);
+        }
+
+        session.getTransaction().commit();
+        session.close();
+
+        HibernateUtil.shutdown();
+    }
+
+}
