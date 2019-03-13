293de9290f52be4c52dc95555f22fdec4f2aebe6
Federico Aloi <federico.aloi@gmail.com>
2019-03-13 16:40:43 -0300

    Tabla con primera letra mayúscula

--- src/main/java/net/isetjb/hibernatetutorial1/Application.java
+++ src/main/java/net/isetjb/hibernatetutorial1/Application.java
@@ -30,24 +30,24 @@ public class Application
 
     private static void borrarProductos(Session session) {
         Transaction transaction = session.beginTransaction();
-        session.createNativeQuery("DELETE FROM product").executeUpdate();
+        session.createNativeQuery("DELETE FROM Product").executeUpdate();
         transaction.commit();
     }
 
     private static void listarProductos(Session session) {
-        List<Object[]> listaProductos = session.createNativeQuery("SELECT * FROM product").getResultList();
+        List<Object[]> listaProductos = session.createNativeQuery("SELECT * FROM Product").getResultList();
         System.out.println("PRODUCTOS");
-        for (Object[] producto : listaProductos) {
-            System.out.println("id: " + producto[0] + ", name: " + producto[1] + ", price: " + producto[2]);
+        for (Object[] Producto : listaProductos) {
+            System.out.println("id: " + Producto[0] + ", name: " + Producto[1] + ", price: " + Producto[2]);
         }
     }
 
     private static void insertarProductos(Session session) {
         Transaction transaction = session.beginTransaction();
         List<String> sqlQueries = Arrays.asList(
-                "INSERT INTO product VALUES(null, 'Yerba La Cumbrecita 500g', 35)",
-                "INSERT INTO product VALUES(null, 'Almidón de Mandioca Arapeguá 1kg', 80)",
-                "INSERT INTO product VALUES(null, 'Uvas rosadas 2kg', 110)");
+                "INSERT INTO Product VALUES(null, 'Yerba La Cumbrecita 500g', 35)",
+                "INSERT INTO Product VALUES(null, 'Almidón de Mandioca Arapeguá 1kg', 80)",
+                "INSERT INTO Product VALUES(null, 'Uvas rosadas 2kg', 110)");
 
         for (String query : sqlQueries) {
             session.createNativeQuery(query).executeUpdate();
